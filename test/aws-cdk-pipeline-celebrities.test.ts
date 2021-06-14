007ef4a3d383e74888439fbb509d75a9d2146699
Robert Dittmann <robert.dittmann@merapar.com>
2021-06-14 10:27:24 +0200

    Initial commit

---
+++ test/aws-cdk-pipeline-celebrities.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsCdkPipelineCelebrities from '../lib/aws-cdk-pipeline-celebrities-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AwsCdkPipelineCelebrities.AwsCdkPipelineCelebritiesStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
